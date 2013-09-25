a9c1bb7eb4d0cf1b0485c850b56faa8621766939
willisj <willisj@uwindsor.ca>
2013-09-25 15:23:54 -0400

    PathTreeMap code cleanup

--- RobotsInterpreter/src/PathTreeMap.java
+++ RobotsInterpreter/src/PathTreeMap.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /* PathTreeMap
 Creator: Joshua Chittle
-Contributors: 
+Contributors: Jordan Willis, code debugging
 
 Description: Represents a sub-tree of a heirarchy mapping to various values.
 Values which decend from this node but are not explicitly given a value inherit this nodes value.
@@ -9,12 +13,13 @@ Values which decend from this node but are not explicitly given a value inherit 
 class PathTreeMap<KeyType extends Comparable<KeyType>, ValueType>
 {
 	private ValueType _value;
-	private List<KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType> > _keyToNextNode;
+	private List<KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType> >> _keyToNextNode;
+	private KeyType currentKey; 
 	
-	public PathTreeMapNode(ValueType value)
+	public PathTreeMap(ValueType value)
 	{
 		this._value = value;
-		this._keyToNextNode = new LinkedList<KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType> >();
+		this._keyToNextNode = new LinkedList< KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType>>>();
 	}
 	
 	public ValueType get()
@@ -29,7 +34,7 @@ class PathTreeMap<KeyType extends Comparable<KeyType>, ValueType>
 			return this.get();
 		}
 		
-		return this.getNextNode(keys[0]).get(Arrays.copyOfRange<KeyType>(keys, 1, keys.length);
+		return ((PathTreeMap<KeyType, ValueType>) this.getNextNode(keys[0])).get(Arrays.copyOfRange(keys, 1, keys.length));
 	}
 	
 	public void put(KeyType[] keys, ValueType value)
@@ -38,31 +43,31 @@ class PathTreeMap<KeyType extends Comparable<KeyType>, ValueType>
 		{
 			this._value = value;
 		}
-		else if(!this.containsKey(keys[0])
+		else if(!this.containsKey(keys[0]))
 		{
-			KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType> element =
-				new KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType>(
-					keys[0], new PathTreeMapNode(this.get()));
+			KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType>> element =
+				new KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType>>(
+					keys[0], new PathTreeMap<KeyType, ValueType>(this.get()));
 			this._keyToNextNode.add(element);
 		}
 		
-		this.getNextNode(keys[0]).put(Arrays.copyOfRange<KeyType>(keys, 1, keys.length), value);
+		((PathTreeMap<KeyType, ValueType>) this.getNextNode(keys[0])).put(Arrays.copyOfRange(keys, 1, keys.length), value);
 	}
 	
 	private ValueType getNextNode(KeyType key)
 	{
-		for(KeyValuePair<KeyType, PathTreeMapNode<KeyType, ValueType> pair : _keyToNextNode)
+		for(KeyValuePair<KeyType, PathTreeMap<KeyType, ValueType>> pair : _keyToNextNode)
 		{
 			if(pair.getKey().equals(currentKey))
 			{
-				return	pair.getValue().get(remainingKeys);
+				return	pair.getValue().get(remainingKeys); 
 			}
 		}
 		
 		return null;
 	}
 	
-	public bool containsKey(KeyType key)
+	private boolean containsKey(KeyType key)
 	{
 		return this.getNextNode(key) != null;
 	}
